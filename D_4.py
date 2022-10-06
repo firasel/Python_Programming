c77ba689fe13cad846b9c3d290e77f127d0985c8
firasel <mdrasel601390@gmail.com>
2022-10-06 20:24:50 +0600

    Python practice

---
+++ D_4.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+x = input("Guess a number: ")
+
+x = input("Multiply 2 with your number: ")
+x = input("Add 8 with the result: ")
+x = input("Divide the whole result with 2: ")
+x = input("Subtract the guessed number with the result: ")
+print("Your gussed number is 4")
