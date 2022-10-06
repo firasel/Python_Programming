da52e8e97943c4340d77064a59b191fa96cbbb9e
firasel <mdrasel601390@gmail.com>
2022-10-07 01:18:03 +0600

    Print pattern unsing loop

--- D_5.py
+++ D_5.py
@@ -1,11 +1,27 @@
-str1 = 'pHitrOn.iO presents "Python COuRSe"'
-print(str1)
-result = ""
-for letter in str1:
-    if letter >= 'A' and letter <= 'Z':
-        result += chr(ord(letter)+32)
-    elif letter >= 'a' and letter <= 'z':
-        result += chr(ord(letter)-32)
-    else:
-        result += letter
-print(result)
+# Problem 1
+# str1 = 'pHitrOn.iO presents "Python COuRSe"'
+# print(str1)
+# result = ""
+# for letter in str1:
+#     if letter >= 'A' and letter <= 'Z':
+#         result += chr(ord(letter)+32)
+#     elif letter >= 'a' and letter <= 'z':
+#         result += chr(ord(letter)-32)
+#     else:
+#         result += letter
+# print(result)
+
+
+# Problem 2
+n = input("Enter a number: ")
+for i in range(1, int(n)+1):
+    for j in range(1, i+1):
+        print(f'{j} ', end="")
+    print()
+
+print()
+# Using one for loop
+str = ""
+for i in range(1, int(n)+1):
+    str += f'{i} '
+    print(str)
