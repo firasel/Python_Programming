09ad4572dc5cb5fd024042209c27da5248e34369
firasel <mdrasel601390@gmail.com>
2022-10-07 01:28:58 +0600

    Display Fibonacci series

--- D_5.py
+++ D_5.py
@@ -13,15 +13,30 @@
 
 
 # Problem 2
-n = input("Enter a number: ")
-for i in range(1, int(n)+1):
-    for j in range(1, i+1):
-        print(f'{j} ', end="")
-    print()
+# n = input("Enter a number: ")
+# for i in range(1, int(n)+1):
+#     for j in range(1, i+1):
+#         print(f'{j} ', end="")
+#     print()
 
-print()
-# Using one for loop
-str = ""
-for i in range(1, int(n)+1):
-    str += f'{i} '
-    print(str)
+# print()
+# # Using one for loop
+# str = ""
+# for i in range(1, int(n)+1):
+#     str += f'{i} '
+#     print(str)
+
+
+# Problem 3
+n = int(input("Enter a number for terms: "))
+prev = 0
+curr = 1
+if n > 0:
+    print(prev, end=" ")
+if n > 1:
+    print(curr, end=" ")
+for i in range(n-2):
+    temp = prev + curr
+    prev = curr
+    curr = temp
+    print(temp, end=" ")
