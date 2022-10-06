42985942218446f1d4b1b20b935aea86a97eff0f
firasel <mdrasel601390@gmail.com>
2022-10-07 01:39:02 +0600

    Count uppercase, lowercase, digits, special symbolbs

--- D_5.py
+++ D_5.py
@@ -28,15 +28,36 @@
 
 
 # Problem 3
-n = int(input("Enter a number for terms: "))
-prev = 0
-curr = 1
-if n > 0:
-    print(prev, end=" ")
-if n > 1:
-    print(curr, end=" ")
-for i in range(n-2):
-    temp = prev + curr
-    prev = curr
-    curr = temp
-    print(temp, end=" ")
+# n = int(input("Enter a number for terms: "))
+# prev = 0
+# curr = 1
+# if n > 0:
+#     print(prev, end=" ")
+# if n > 1:
+#     print(curr, end=" ")
+# for i in range(n-2):
+#     temp = prev + curr
+#     prev = curr
+#     curr = temp
+#     print(temp, end=" ")
+
+
+# Problem 4
+str1 = "P@#yn26at^&i5ve"
+upCase = 0
+lwCase = 0
+digits = 0
+symbols = 0
+
+for letter in str1:
+    if letter >= 'A' and letter <= 'Z':
+        upCase += 1
+    elif letter >= 'a' and letter <= 'z':
+        lwCase += 1
+    elif letter >= '0' and letter <= '9':
+        digits += 1
+    else:
+        symbols += 1
+
+print(
+    f'UpperCase = {upCase}\nLowerCase = {lwCase}\nDigits = {digits}\nSymbol = {symbols}')
