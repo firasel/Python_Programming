1dfd414c349a67dca440e642eb45307e71601a8c
firasel <mdrasel601390@gmail.com>
2022-10-10 17:04:06 +0600

    List, set, tuple practice

---
+++ D_7.py
@@ -0,0 +1,14 @@
+numbers = [12, 34, 45, 12, 45, 78, 89, 90]
+
+print(numbers[1])
+print(numbers[-6])
+print(numbers[5:-1])
+print(numbers)
+# Set
+nums = {12, 34, 45, 12, 45, 78, 89, 90}
+nums2 = set(numbers)
+print(nums)
+print(nums2)
+
+nums_tuple = 12, 34, 45, 12, 45, 78, 89, 90
+print(nums_tuple)
