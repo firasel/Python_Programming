cb9f268cbcce159cc649fd3c3ea3431d9f448731
firasel <mdrasel601390@gmail.com>
2022-10-10 19:18:58 +0600

    Map, filter, lambda practice

--- D_7.py
+++ D_7.py
@@ -32,3 +32,20 @@ for mark in marks:
 
 for i, num in enumerate(numbers):
     print(i, num)
+
+
+def square(x): return x*x
+
+
+def double_it(x): return x*2
+
+
+print(square(5))
+print(double_it(4))
+double_numbers = map(double_it, numbers)
+double_numbers2 = list(map(lambda x: x*x, numbers))
+print(list(double_numbers))
+print(double_numbers2)
+
+bigger_numbers = list(filter(lambda x: x > 1000, double_numbers2))
+print(bigger_numbers)
