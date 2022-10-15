28f141e1af55130e97e80b180ab54e7665adbec6
firasel <mdrasel601390@gmail.com>
2022-10-15 17:20:16 +0600

    Assignment complete

--- D_10.py
+++ D_10.py
@@ -1,8 +1,29 @@
-import pyjokes
+a = ['oh', 'Emelia', 'to']
+
+s = "Oh, I got two tickets for Dhaka. I and Emelia love to visit different places very much. This time we are going to Bangladesh."
 
 
-def tell_some_jokes():
-    return pyjokes.get_joke()
+def create_new_string():
+    output = s.replace(',', "")
+    output = output.replace('.', "")
+    output = output.split(' ')
+    output.reverse()
+    result = []
+
+    for i in output:
+        for j in a:
+            if i.capitalize() == j.capitalize():
+                index = output.index(i)
+                if output[index-1] not in result:
+                    result.append(output[index-1])
+
+    result.reverse()
+
+    with open("out.txt", "w") as out:
+        for word in result:
+            out.write(word)
+            out.write(' ')
+        out.close()
 
 
-print(tell_some_jokes())
+create_new_string()
