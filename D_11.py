b87413f9481339581e489e3930375208f52ae725
firasel <mdrasel601390@gmail.com>
2022-10-16 15:06:24 +0600

    Bank system with Class in python

--- D_11.py
+++ D_11.py
@@ -1,17 +1,29 @@
-class Shop:
-    cart = []
+class Bank:
+    def __init__(self, balance):
+        self.balance = balance
+        self.min_withdraw = 100
+        self.max_withdraw = 10000
 
-    def __init__(self, buyer):
-        self.buyer = buyer
+    def get_balance(self):
+        return self.balance
 
-    def addToCart(self, item):
-        self.cart.append(item)
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount < self.min_withdraw:
+            return f'no money for you. Minimum withdraw: {self.min_withdraw}'
+        elif amount > self.max_withdraw:
+            return f'you crossed max limit: {self.max_withdraw}'
+        elif amount > self.balance:
+            return 'You are broke!!! No money for you'
+        else:
+            self.balance = self.balance - amount
+            return f'Here is your money: {amount}'
 
 
-shopper_1 = Shop("SS1")
-shopper_1.addToCart("t-shirt")
-print(shopper_1.cart)
-
-shopper_2 = Shop("SS2")
-shopper_2.addToCart("shoes")
-print(shopper_2.cart)
+my_bank = Bank(20000)
+print(my_bank.withdraw(1000))
+print(my_bank.get_balance())
+my_bank.deposit(2000)
+print(my_bank.get_balance())
