a290ecc76c06fbf2378616f19d91db5a3a8ea6a0
firasel <mdrasel601390@gmail.com>
2022-10-17 08:06:03 +0600

    Decorator with class practice

--- D_12.py
+++ D_12.py
@@ -1,22 +1,34 @@
-import math
-from time import time
+class User:
+    def __init__(self, f_name, l_name):
+        self.first = f_name
+        self.last = l_name
+        self.email = f'{self.first}.{self.last}@user.com'
+
+    @property
+    def full_name(self):
+        return f'{self.first} {self.last}'
+
+    @full_name.setter
+    def full_name(self, value):
+        first, last = value.split(' ')
+        self.first = first
+        self.last = last
+        self.email = f'{first}.{last}@user.com'
+
+    @full_name.deleter
+    def full_name(self):
+        del self.first
+        del self.last
+
+    def get_email(self):
+        return self.email
 
 
-def timer(func):
-    def inner(*args, **kwargs):
-        print('Time Start')
-        start = time()
-        func(*args, **kwargs)
-        end = time()
-        print(f'Time End. Total time taken {end - start}')
-    return inner
-
-
-@timer
-def get_factorial(n):
-    result = math.factorial(n)
-    print(f'Factorial of {n} is: {result}')
-
-
-# timer(get_factorial)()
-get_factorial(n=20)
+mark = User('md', 'sahil')
+print(mark.get_email())
+print(mark.full_name)
+mark.full_name = "Md sohel"
+print(mark.full_name)
+print(mark.get_email())
+# del mark.full_name
+print(mark.full_name)
