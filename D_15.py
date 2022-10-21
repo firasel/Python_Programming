d983a35c9f259413bf2e350b55452eacb7746969
firasel <mdrasel601390@gmail.com>
2022-10-21 09:43:15 +0600

    Assignment problem 2 solved

--- D_15.py
+++ D_15.py
@@ -1,9 +1,5 @@
-def exp(a, n):
-    res = 1
-    for i in range(n):
-        res *= a
-    return res
-
-
-a, n = [int(num) for num in input("enter numbers: ").split()]
-print("result is:", exp(a, n))
+sum = 0
+nums = [float(num) for num in input("enter numbers: ").split('-')]
+for num in nums:
+    sum += num
+print("sum is:", sum)
