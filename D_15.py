121a5e4f611c02f554c2281ec4211eaa0284ba9c
firasel <mdrasel601390@gmail.com>
2022-10-21 09:44:33 +0600

    Assignment problem 5 solved

--- D_15.py
+++ D_15.py
@@ -1,9 +1,10 @@
-s = input("enter string: ")
-result = []
-for i in range(0, len(s), 2):
-    temp = ""
-    for j in range(int(s[i+1])):
-        temp += s[i]
-    result.append(temp)
-result = "".join(sorted(result, key=str.lower))
-print("result is: ", result)
+print("Enter a blank student name and the program will end.")
+while True:
+    student_name = input("\nStudent Name: ")
+    if student_name == "":
+        break
+    mark = int(input("Mark: "))
+    f = open('studentData.txt', 'r+')
+    student_id = len(f.readlines())+1
+    f.write(f'Id: {student_id}, Name: {student_name}, Mark: {mark}\n')
+    f.close()
