264917bc14f0badc7b6f38bb63ac0deb745173bc
firasel <mdrasel601390@gmail.com>
2022-10-21 12:12:09 +0600

    Assignment problem 10 solved

--- D_15.py
+++ D_15.py
@@ -1,17 +1,20 @@
-class Calculate:
-    def __init__(self, x, n):
-        self.x = x
-        self.n = n
-
-    def sum(self):
-        return self.x+self.n
-
-    def pow(self):
-        res = 1
-        for i in range(self.n):
-            res *= self.x
-        return res
+import math
 
 
-calculator = Calculate(3, 3)
-print(calculator.sum(), calculator.pow())
+class Distance:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def calculate(self):
+        return math.sqrt(math.pow(self.x2-self.x1, 2)+math.pow(self.y2-self.y1, 2)*1.0)
+
+
+x1 = 3
+y1 = 4
+x2 = 7
+y2 = 8
+calculator = Distance(x1, y1, x2, y2)
+print("%.2f" % calculator.calculate())
