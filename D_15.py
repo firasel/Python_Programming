dfec01776df1b2cfa4c580f56fc716ecdd760209
firasel <mdrasel601390@gmail.com>
2022-10-21 11:29:09 +0600

    Assignment problem 8 solved

--- D_15.py
+++ D_15.py
@@ -1,5 +1,20 @@
-def func(arg1, arg2, arg3=4, arg4=5):
-    print(arg1, arg2, arg3, arg4)
+class PairElement:
+    def __init__(self):
+        self.indx = 1
+        self.result = [-1, -1]
+
+    def targetSum(self, l1, target):
+        if l1[0]+l1[1] == target:
+            self.result[0] = self.indx
+            self.result[1] = self.indx+1
+        else:
+            if len(l1) > 2:
+                self.indx += 1
+                self.targetSum(l1[1:], target)
+        return self.result
 
 
-func(3, 4, arg2=1)
+a = [10, 20, 10, 40, 50, 60, 70]
+target = 50
+result = PairElement().targetSum(a, target)
+print(result[0], result[1])
