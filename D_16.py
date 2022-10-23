80403429366254908c6185fa25c83ad6f8a7f76c
firasel <mdrasel601390@gmail.com>
2022-10-23 19:06:36 +0600

    Multiple inheritance practice

--- D_16.py
+++ D_16.py
@@ -1,41 +1,23 @@
-# inheritance
-class Vehicle:
-    def __init__(self, name, license, price) -> None:
+# Multiple inheritance
+class School:
+    def __init__(self, school_name) -> None:
+        self.school_name = school_name
+        print("School init called")
+
+
+class Grade:
+    def __init__(self, grade_name) -> None:
+        self.grade_name = grade_name
+        print("Grade class init called")
+
+
+class Student(School, Grade):
+    def __init__(self, name, grade_name, school_name) -> None:
         self.name = name
-        self.license = license
-        self.price = price
-        print("Init called from Vehicle")
-
-    def start(self):
-        print(f'{self.name} started')
+        print("Student init called")
+        School.__init__(self, school_name)
+        Grade.__init__(self, grade_name)
 
 
-class Bus(Vehicle):
-    def __init__(self, name, license, price, seat, ticket_price) -> None:
-        print("Init called from Bus")
-        super().__init__(name, license, price)
-        self.seat = seat
-        self.available_seats = seat
-        self.ticket_price = ticket_price
-
-    def sell_ticket(self, customer_name, quantity, amount):
-        self.available_seats -= quantity
-        remainder = amount - self.ticket_price * quantity
-        if remainder >= 0:
-            return Ticket(customer_name)
-        return 'no ticket for you'
-
-
-class Ticket:
-    def __init__(self, owner) -> None:
-        self.owner = owner
-
-
-class AcBus(Bus):
-    def __init__(self, name, license, price, seat, ticket_price) -> None:
-        print("Init called from ACBus")
-        super().__init__(name, license, price, seat, ticket_price)
-
-
-city_bus = AcBus("CityBus", "JKP023", 2500000, 50, 500)
-print(city_bus.__dict__)
+david = Student('David', 'BSC', 'AB School')
+print(david.__dict__)
