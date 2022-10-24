d2124ae9bd4b890d7bf1b627021909c8cd7b7778
firasel <mdrasel601390@gmail.com>
2022-10-24 19:46:44 +0600

    Encapsulation practice

--- D_17.py
+++ D_17.py
@@ -1,28 +1,30 @@
-from abc import ABC, abstractmethod
+class Account:
+    def __init__(self, holder, initial_balance):
+        self.holder = holder
+        self.__balance = initial_balance
+
+    def deposit(self, amount):
+        print(f'Adding {amount} to your account')
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        print(f'Withdrawing {amount} from your account')
+        self.__balance -= amount
+
+    def get_balance(self):
+        return self.__balance
 
 
-# Abstract base class
-class Animal(ABC):
-    @abstractmethod
-    def eat(self):
-        pass
-
-    def name(self):
-        pass
-
-    def move(self):
-        print("moving around in the zoo")
+class StudentAccount(Account):
+    def __init__(self, holder, initial_balance, school):
+        self.school = school
+        super().__init__(holder, initial_balance)
 
 
-class Monkey(Animal):
-    def sing(self):
-        print('monkey is signing')
-
-    def eat(Self):
-        print("eating banana")
-
-
-lyka = Monkey()
-print(lyka)
-lyka.eat()
-lyka.move()
+rashid = StudentAccount('Rashid', 60000, 'DIU')
+print(rashid.get_balance())
+rashid.deposit(20000)
+rashid.deposit(10000)
+print(rashid.get_balance())
+rashid.withdraw(5000)
+print(rashid.get_balance())
