1739280b1fc4673b7854d9531e4bbe50234e5ecc
firasel <mdrasel601390@gmail.com>
2022-10-25 10:30:03 +0600

    Deleter and readonly property

--- D_17.py
+++ D_17.py
@@ -1,37 +1,26 @@
-class Account:
-    def __init__(self, holder, initial_balance):
-        self.holder = holder
-        self.__balance = initial_balance
+class Student:
+    def __init__(self, name, id, marks) -> None:
+        self._name = name
+        self.__id = id
+        self.marks = marks
 
-    def __add__(self, other):
-        return self.__balance + other.__balance
+    @property
+    def student_id(self):
+        return self.__id
 
-    def deposit(self, amount):
-        print(f'Adding {amount} to your account')
-        self.__balance += amount
+    @property
+    def name(self):
+        return self._name
 
-    def withdraw(self, amount):
-        print(f'Withdrawing {amount} from your account')
-        self.__balance -= amount
-
-    def get_balance(self):
-        return self.__balance
-
-
-class StudentAccount(Account):
-    def __init__(self, holder, initial_balance, school):
-        self.school = school
-        super().__init__(holder, initial_balance)
+    @name.deleter
+    def name(self):
+        del self._name
 
 
-rashid = StudentAccount('Rashid', 60000, 'DIU')
-rashid2 = StudentAccount('Rashid 2', 6000, 'DIU')
+rashid = Student('Rashid', 25, 60)
 
-print(rashid.get_balance())
-rashid.deposit(20000)
-rashid.deposit(10000)
-print(rashid.get_balance())
-rashid.withdraw(5000)
-print(rashid.get_balance())
-
-print(rashid+rashid2)
+print(rashid.student_id)
+print(rashid.name)
+print(rashid.__dict__)
+del rashid.name
+print(rashid.__dict__)
