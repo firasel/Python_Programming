ce385ade6cd5fcb79e075dd5585c55e8d138c276
firasel <mdrasel601390@gmail.com>
2022-11-01 17:19:25 +0600

    Rider app almost complete

--- D_21.py
+++ D_21.py
@@ -1,6 +1,7 @@
 import hashlib
+import threading
 from pdb import line_prefix
-from random import randint
+from random import choice, randint
 
 from D_21_Brta import BRTA
 from D_21_RideManager import uber
@@ -25,7 +26,6 @@ class User:
             if flag:
                 file.write(f'{email} {pwd_encrypted}\n')
         file.close()
-        # print(self.name, "User created")
 
     @staticmethod
     def log_in(email, password):
@@ -56,17 +56,13 @@ class Rider(User):
     def get_location(self):
         return self.location
 
-    def request_trip(self, destination):
-        pass
-
     def get_trip_history(self):
         return self.__trip_history
 
     def start_a_trip(self, fare, trip_info):
+        print(f'A trip started for {self.name}')
         self.balance -= fare
         self.__trip_history.append(trip_info)
-
-    pass
 
 
 class Driver(User):
@@ -77,11 +73,11 @@ class Driver(User):
         self.license = license
         self.valid_driver = license_authority.validate_license(email, license)
         self.earning = 0
+        self.vehicle = None
 
     def take_driving_test(self):
         result = license_authority.take_driving_test(self.email)
         if result == False:
-            # print("Sorry you failed, try again")
             self.license = None
         else:
             self.license = result
@@ -90,27 +86,30 @@ class Driver(User):
     def register_a_vehicle(self, vehicle_type, license_plate, rate):
         if self.valid_driver is True:
             if vehicle_type == 'car':
-                new_vehicle = Car(
+                self.vehicle = Car(
                     vehicle_type, license_plate, rate, self)
-                uber.add_a_vehicle('car', new_vehicle)
+                uber.add_a_vehicle('car', self.vehicle)
             elif vehicle_type == 'bike':
-                new_vehicle = Bike(
+                self.vehicle = Bike(
                     vehicle_type, license_plate, rate, self)
-                uber.add_a_vehicle('bike', new_vehicle)
+                uber.add_a_vehicle('bike', self.vehicle)
             else:
-                new_vehicle = Cng(
+                self.vehicle = Cng(
                     vehicle_type, license_plate, rate, self)
-                uber.add_a_vehicle('cng', new_vehicle)
+                uber.add_a_vehicle('cng', self.vehicle)
         else:
             pass
-            # print("You are not a valid driver")
 
     def get_trip_history(self):
         return self.__trip_history
 
-    def start_a_trip(self, destination, fare, trip_info):
+    def start_a_trip(self, start, destination, fare, trip_info):
         self.earning += fare
         self.location = destination
+        # Start thread
+        trip_thread = threading.Thread(
+            target=self.vehicle.start_driving, args=(start, destination))
+        trip_thread.start()
         self.__trip_history.append(trip_info)
 
 
@@ -118,16 +117,20 @@ rider1 = Rider('rider1', 'rider1@gmail.com', 'rider1', randint(0, 30), 1000)
 rider2 = Rider('rider2', 'rider2@gmail.com', 'rider2', randint(0, 30), 5000)
 rider3 = Rider('rider3', 'rider3@gmail.com', 'rider3', randint(0, 30), 5000)
 
+vehicle_types = ['car', 'bike', 'cng']
+
 for i in range(1, 100):
     driver1 = Driver(f'driver{i}', f'driver{i}@gmail.com',
                      f'driver{i}', randint(0, 100), randint(1000, 9999))
     driver1.take_driving_test()
-    driver1.register_a_vehicle('car', randint(10000, 99999), 10)
+    driver1.register_a_vehicle(
+        choice(vehicle_types), randint(10000, 99999), 10)
 
 
-uber.find_a_vehicle(rider1, 'car', randint(1, 100))
-uber.find_a_vehicle(rider1, 'car', randint(1, 100))
-uber.find_a_vehicle(rider1, 'car', randint(1, 100))
-uber.find_a_vehicle(rider1, 'car', randint(1, 100))
-
-print(rider1.get_trip_history())
+uber.find_a_vehicle(rider1, choice(vehicle_types), randint(1, 100))
+uber.find_a_vehicle(rider2, choice(vehicle_types), randint(1, 100))
+uber.find_a_vehicle(rider3, choice(vehicle_types), randint(1, 100))
+uber.find_a_vehicle(rider1, choice(vehicle_types), randint(1, 100))
+uber.find_a_vehicle(rider2, choice(vehicle_types), randint(1, 100))
+uber.find_a_vehicle(rider3, choice(vehicle_types), randint(1, 100))
+uber.find_a_vehicle(rider2, choice(vehicle_types), randint(1, 100))
