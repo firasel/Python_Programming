7224cb53a576b4c6d88cb3487c8ca276398713a4
firasel <mdrasel601390@gmail.com>
2022-11-03 17:19:34 +0600

    Practice problem 3 solved

--- D_23.py
+++ D_23.py
@@ -1,17 +1,8 @@
-exStr = "We tried list and we tried dicts also we tried Zen"
+exStr = "GATGGAACTTGACTACGTAAATT"
 
 
-def countWord(wordArr):
-    wordDict = {}
-    for word in wordArr:
-        if word in wordDict.keys():
-            wordDict[word] = wordDict[word]+1
-        else:
-            wordDict[word] = 1
-    return wordDict
+def DNAtoRNA(exStr):
+    return str.replace(exStr, 'T', 'U')
 
 
-wordArr = exStr.split(' ')
-wordDict = countWord(wordArr)
-for key, val in wordDict.items():
-    print(f'{key}\t{val}')
+print(DNAtoRNA(exStr))
