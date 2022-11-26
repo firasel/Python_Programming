e7cd4bf0fa95caa06fabf0afd85bb93405340575
firasel <mdrasel601390@gmail.com>
2022-11-26 14:58:10 +0600

    Recursion to iterative convert

--- D_32/main.py
+++ D_32/main.py
@@ -1,23 +1,11 @@
-def half_adder(a, b):
-    s = a ^ b
-    c = a & b
-    print(f'Sum:\t{s}\nCarry:\t{c}')
-    return [s, c]
+def sum_odd(n):
+    total = 0
+    if (n % 2 == 0):
+        n -= 1
+    for i in range(3, n+1, 2):
+        total += i
+    return total
 
 
-if __name__ == '__main__':
-    half_adder(0, 1)
-
-
-def test_half_adder():
-    t1 = half_adder(0, 0)
-    assert t1[0] == 0 and t1[1] == 0
-
-    t2 = half_adder(0, 1)
-    assert t2[0] == 1 and t2[1] == 0
-
-    t3 = half_adder(1, 0)
-    assert t3[0] == 1 and t3[1] == 0
-
-    t4 = half_adder(1, 1)
-    assert t4[0] == 0 and t4[1] == 1
+print(sum_odd(10))
+print(sum_odd(5))
