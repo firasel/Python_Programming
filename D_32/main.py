14ed22be9c2c0dbd80b12c184311f8cdaa897a84
firasel <mdrasel601390@gmail.com>
2022-11-26 14:44:11 +0600

    Test code for half-adder function

--- D_32/main.py
+++ D_32/main.py
@@ -7,3 +7,17 @@ def half_adder(a, b):
 
 if __name__ == '__main__':
     half_adder(0, 1)
+
+
+def test_half_adder():
+    t1 = half_adder(0, 0)
+    assert t1[0] == 0 and t1[1] == 0
+
+    t2 = half_adder(0, 1)
+    assert t2[0] == 1 and t2[1] == 0
+
+    t3 = half_adder(1, 0)
+    assert t3[0] == 1 and t3[1] == 0
+
+    t4 = half_adder(1, 1)
+    assert t4[0] == 0 and t4[1] == 1
