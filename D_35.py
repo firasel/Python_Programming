d04fcae9ee8156c3d3600585c6deda5ebb4c78f9
firasel <mdrasel601390@gmail.com>
2022-12-02 20:12:33 +0600

    Problem 5, find bugs and fix that

--- D_35.py
+++ D_35.py
@@ -1,2 +1,4 @@
-data = {key: [val for val in range(1, 6) if val != key] for key in range(1, 6)}
-print(data)
+data = [{'a': 5, 'b': 10}, {'x': 15, 'y': 20}]
+for val in range(len(data)):
+    for key, val2 in data[val].items():
+        print(f"Key:{key} Value:{val2}")
