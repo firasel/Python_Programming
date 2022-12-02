3fad5d072ca5dfeb9b1499c737ffb0686dc5e51e
firasel <mdrasel601390@gmail.com>
2022-12-02 20:58:25 +0600

    Problem 9, Run functions in threads

--- D_35.py
+++ D_35.py
@@ -1,4 +1,32 @@
-def even_odd(num): return "Yes" if num % 2 == 0 else "No"
+import threading
 
 
-print(even_odd(4))
+def f1():
+    for i in range(5):
+        print(f"f1 - {i}")
+
+
+def f2():
+    for i in range(5):
+        print(f"f2 - {i}")
+
+
+def f3():
+    for i in range(5):
+        print(f"f3 - {i}")
+
+
+def f4():
+    for i in range(5):
+        print(f"f4 - {i}")
+
+
+th1 = threading.Thread(target=f1)
+th2 = threading.Thread(target=f2)
+th3 = threading.Thread(target=f3)
+th4 = threading.Thread(target=f4)
+
+th1.start()
+th2.start()
+th3.start()
+th4.start()
