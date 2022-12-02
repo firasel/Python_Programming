593c9ebc9ef5924b6b4d3e33d4e5c1502aed6c6c
firasel <mdrasel601390@gmail.com>
2022-12-02 20:42:24 +0600

    Problem 8, even odd number check with lambda function

--- D_35.py
+++ D_35.py
@@ -1,3 +1,4 @@
-lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-newList = [val for val in lst if val % 3 == 0]
-print(newList)
+def even_odd(num): return "Yes" if num % 2 == 0 else "No"
+
+
+print(even_odd(4))
