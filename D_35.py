02ac4d3f4de4d813fa5af99256a28cfce5adc4e2
firasel <mdrasel601390@gmail.com>
2022-12-04 20:30:33 +0600

    Final exam problem 3 solved

--- D_35.py
+++ D_35.py
@@ -1,32 +1,32 @@
-import threading
+# Global Scope
+# The Variable which can be read from anywhere in the program is known as a global scope. These variables can be accessed inside and outside the function. Here num=1 is a global variable.
+
+# Local Scope
+# The Variables which are defined in the function are a local scope of the variable. Here num=5 is a local variable that is declared and printed inside the function getNum.
+
+# Nonlocal Scope
+# Nonlocal Variable is the variable that is defined in the nested function. Here num=10 is a nonlocal variable.
+
+# Built-in Scope
+# If a Variable is not defined in local, Enclosed or global scope, then python looks for it in the built-in scope. Here pi is imported from math, and the value of pi is not defined in global, local and enclosed.
+
+from math import pi
+
+num = 1
+print("Global:", num)
 
 
-def f1():
-    for i in range(5):
-        print(f"f1 - {i}")
+def getNum():
+    num = 5
+    print("GetNum:", num)
+
+    def inner():
+        nonlocal num
+        num = 10
+        print("Inner:", num)
+    inner()
+    print("GetNum:", num)
 
 
-def f2():
-    for i in range(5):
-        print(f"f2 - {i}")
-
-
-def f3():
-    for i in range(5):
-        print(f"f3 - {i}")
-
-
-def f4():
-    for i in range(5):
-        print(f"f4 - {i}")
-
-
-th1 = threading.Thread(target=f1)
-th2 = threading.Thread(target=f2)
-th3 = threading.Thread(target=f3)
-th4 = threading.Thread(target=f4)
-
-th1.start()
-th2.start()
-th3.start()
-th4.start()
+getNum()
+print("Built-in:", pi)
